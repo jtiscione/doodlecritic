5ee2109d400708855e13939148af7c75928dcfe4
jason Tiscione <tiscione@gmail.com>
2019-06-27 18:12:18 -0600

    Completed TagCloud component teardown, now a stateless component

--- src/TagCloud.js
+++ src/TagCloud.js
@@ -2,24 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import arrayShuffle from 'shuffle-array';
 import randomColor from 'randomcolor';
-import { fontSizeConverter, arraysEqual, propertiesEqual } from './helpers';
 
-class TagCloud extends React.Component {
+function TagCloud({tags}) {
 
-  componentWillReceiveProps(newProps) {
-    const propsEqual = propertiesEqual(this.props, newProps, Object.keys(TagCloud.propTypes))
-    const tagsEqual = arraysEqual(newProps.tags, this.props.tags);
-    if (!tagsEqual || !propsEqual) {
-      this._populate(newProps);
-    }
-  }
+  const minSize = 12;
+  const maxSize = 35;
 
-  componentWillMount() {
-    this._populate(this.props);
-  }
+  const counts = tags.map(tag => tag.count),
+    min = Math.min(...counts),
+    max = Math.max(...counts);
 
-  render() {
-    const tagElements = this._data.map(({tag, fontSize, color}) => {
+  const data = tags.map(tag => ({
+    tag,
+    color: tag.color || randomColor({
+      luminosity: 'light',
+      hue: 'orange',
+    }),
+    fontSize: minSize + Math.round(((tag.count - min) * (maxSize - minSize)) / (max - min)),
+  }));
+
+  const tagElements = arrayShuffle(data, { copy: true, rnd: null })
+    .map(({tag, fontSize, color}) => {
       const key = tag.key || tag.value;
       const style = {
         margin: '0px 3px',
@@ -30,32 +33,7 @@ class TagCloud extends React.Component {
       };
       return <span style={style} key={key}>{tag.value}</span>;
     });
-    return (
-      <div>
-        { tagElements }
-      </div>
-    )
-  }
-  _populate(props) {
-    const { tags } = props;
-
-    const minSize = 12;
-    const maxSize = 35;
-
-    const counts = tags.map(tag => tag.count),
-      min = Math.min(...counts),
-      max = Math.max(...counts);
-    const data = tags.map(tag => ({
-      tag,
-      color: tag.color || randomColor({
-        luminosity: 'light',
-        hue: 'orange',
-      }),
-      fontSize: fontSizeConverter(tag.count, min, max, minSize, maxSize)
-    }));
-    this._data = arrayShuffle(data, { copy: true, rnd: null });
-  }
-
+  return <div>{ tagElements }</div>;
 }
 
 TagCloud.propTypes = {
